ed461e47afacbab3969b35296171905000616014
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-02-28 07:07:07 -0800

    Add test to verify the correct size of the embedding

--- birdclef/models/embedding/tilenet.py
+++ birdclef/models/embedding/tilenet.py
@@ -14,7 +14,7 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from nnAudio import Spectrogram
+from nnAudio.features.mel import MelSpectrogram
 from torch.autograd import Variable
 
 
@@ -61,17 +61,19 @@ class TileNet(pl.LightningModule):
         self.z_dim = z_dim
         self.in_planes = 64
 
-        self.spec_layer = Spectrogram.MelSpectrogram(
+        self.spec_layer = MelSpectrogram(
             n_mels=n_mels, hop_length=hop_length, fmin=fmin, fmax=fmax, sr=sample_rate
         )
+        print(self.spec_layer)
 
         self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(128, num_blocks[1], stride=2)
-        # self.layer3 = self._make_layer(256, num_blocks[2], stride=2)
-        # self.layer4 = self._make_layer(512, num_blocks[3], stride=2)
+        self.layer3 = self._make_layer(256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(512, num_blocks[3], stride=2)
         self.layer5 = self._make_layer(self.z_dim, num_blocks[4], stride=2)
+        self.fc1 = nn.Linear(self.z_dim * 3, self.z_dim)
 
     def _make_layer(self, planes, num_blocks, stride, no_relu=False):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -86,11 +88,13 @@ class TileNet(pl.LightningModule):
         x = F.relu(self.bn1(self.conv1(x.unsqueeze(1))))
         x = self.layer1(x)
         x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
         x = self.layer5(x)
         x = F.avg_pool2d(x, 4)
         z = x.view(x.size(0), -1)
+        # for some reason, we get 3x the size of the final dimension
+        z = self.fc1(z)
         return z
 
     def forward(self, x):
--- tests/test_model_embed_tilenet.py
+++ tests/test_model_embed_tilenet.py
@@ -1,10 +1,28 @@
 import pytorch_lightning as pl
+from torchsummary import summary
 
 from birdclef.models.embedding import datasets, tilenet
 
 
 def test_tilenet_train(metadata_df, tile_path):
-    data_module = datasets.TileTripletsDataModule(metadata_df, tile_path, batch_size=1)
-    model = tilenet.TileNet(z_dim=512, n_mels=64)
+    z_dim = 64
+
+    # test that the model actually runs
+    data_module = datasets.TileTripletsDataModule(
+        metadata_df,
+        tile_path,
+        batch_size=1,
+        num_workers=1,
+    )
+    model = tilenet.TileNet(z_dim=z_dim, n_mels=64)
     trainer = pl.Trainer(fast_dev_run=True)
     trainer.fit(model, data_module)
+
+    # assert the shape of the data. Since we're predicting, we'll only keep the
+    # anchor from the batch
+    for batch in data_module.test_dataloader():
+        x = batch["anchor"]
+        yhat = model(x)
+    # print out the summary from the last batch
+    summary(model, x)
+    assert yhat.shape[1] == z_dim
