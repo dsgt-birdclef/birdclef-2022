ee0a755243821263a42dc34f0c53bc94345b3516
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-03-09 20:20:58 -0800

    Bump verison to 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="birdclef",
-    version="0.3.1",
+    version="0.4.0",
     description="Utilities for birdclef",
     author="Anthony Miyaguchi",
     author_email="acmiyaguchi@gmail.com",
