27c74bf8d7217e66e150e1abbea7f88f17e7b7cd
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2022-05-27 21:28:03 -0700

    Update evaluation with more intra species plots and audio

--- birdclef/workflows/evaluation.py
+++ birdclef/workflows/evaluation.py
@@ -1,21 +1,20 @@
-import importlib.resources as pkg_resources
-import os
 import webbrowser
-from datetime import date
+from datetime import datetime
 from pathlib import Path
 
 import click
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import soundfile as sf
 import torch
-from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader, select_autoescape
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import LabelEncoder
 
 from birdclef.models.embedding.tilenet import TileNet
-from birdclef.workflows import motif, templates
+from birdclef.utils import compute_offset, load_audio
 
 ROOT = Path(__file__).parent.parent.parent
 
@@ -25,15 +24,110 @@ def evaluation():
     pass
 
 
+def intra_cluster(base_path, root, df, model, intra):
+    data = []
+    for row in df[df.species == intra].sample(50).itertuples():
+        try:
+            offset = compute_offset(
+                row.motif_0,
+                row.matrix_profile_window,
+                row.duration_cens,
+                row.duration_seconds,
+            )[0]
+        except:
+            offset = 0
+        y = load_audio(root / row.source_name, offset, 5)
+        data.append(y)
+
+    figures = []
+
+    # plot the waveform of the first data point
+    for i in range(3):
+        z = data[i]
+        plt.plot(z)
+        name = f"{intra}_intra_waveform_{i}.png"
+        figures.append(name)
+        plt.savefig(f"{base_path}/{name}")
+        plt.clf()
+        sf.write(
+            base_path / name.replace(".png", ".ogg"),
+            z,
+            32000,
+            format="ogg",
+            subtype="vorbis",
+        )
+
+    # plot the embedding in an even lower dimension
+    emb = model(torch.from_numpy(np.array(data))).detach().numpy()
+    g = PCA(n_components=2).fit_transform(emb)
+
+    name = f"{intra}_intraclass.png"
+    plt.scatter(g[:, 0], g[:, 1])
+    plt.savefig(f"{base_path}/{name}")
+    plt.clf()
+    figures.append(name)
+
+    kmeans = KMeans(n_clusters=3).fit(emb)
+    name = f"{intra}_intraclass_colored.png"
+    plt.scatter(g[:, 0], g[:, 1], c=kmeans.labels_)
+    plt.savefig(f"{base_path}/{name}")
+    plt.clf()
+    figures.append(name)
+
+    return figures
+
+
+def inter_cluster(base_path, root, df, model, species: "list[str]", num_sample=300):
+    data = []
+    labels = []
+    for specie in species:
+        for row in df[df.species == specie].sample(num_sample).itertuples():
+            try:
+                offset = compute_offset(
+                    row.motif_0,
+                    row.matrix_profile_window,
+                    row.duration_cens,
+                    row.duration_seconds,
+                )[0]
+            except:
+                offset = 0
+            y = load_audio(root / row.source_name, offset, 5)
+            data.append(y)
+            labels.append(row.species)
+
+    figures = []
+    for i in range(3):
+        z = data[i]
+        plt.plot(z)
+        name = f"inter_waveform_{i}.png"
+        figures.append(name)
+        plt.savefig(f"{base_path}/{name}")
+        plt.clf()
+        sf.write(
+            base_path / name.replace(".png", ".ogg"),
+            z,
+            32000,
+            format="ogg",
+            subtype="vorbis",
+        )
+
+    emb = model(torch.from_numpy(np.array(data))).detach().numpy()
+    le = LabelEncoder().fit(labels)
+
+    g = PCA(n_components=2).fit_transform(emb)
+    plt.scatter(g[:, 0], g[:, 1], c=le.transform(labels))
+    name = f"interclass.png"
+    plt.savefig(f"{base_path}/{name}")
+    plt.clf()
+    figures.append(name)
+
+    return figures
+
+
 @evaluation.command()
 @click.option(
-    "--intra",
-    default="brnowl",
-    help="Singular species to perform intraclass inspection",
-)
-@click.option(
-    "--inter",
-    default="brnowl,skylar,norcar",
+    "--species",
+    default="brnowl,skylar,houfin",
     help="Array of 3 species to perform interclass comparison",
 )
 @click.option(
@@ -47,8 +141,8 @@ def evaluation():
     default=Path("data/processed/2022-04-03-motif-consolidated.parquet"),
 )
 @click.option(
-    "--outputdir",
-    type=click.Path(exists=True, dir_okay=True),
+    "--output-path",
+    type=click.Path(file_okay=False),
     default=Path(f"{ROOT}/data/intermediate/generated_resources"),
 )
 @click.option(
@@ -57,64 +151,27 @@ def evaluation():
     default=Path("data/raw/birdclef-2022"),
 )
 @click.option("--dim", default=512)
-@click.option("--name", default=date.today())
-def main(intra, inter, checkpoint, parquet, outputdir, root, dim, name):
-    model = TileNet.load_from_checkpoint(checkpoint, dim=512)
+@click.option("--experiment-name", default=datetime.utcnow().strftime("%Y%m%d%H%M"))
+def clustering(species, checkpoint, parquet, output_path, root, dim, experiment_name):
+    output_path = Path(output_path)
+    species = species.split(",")
+
+    base_path = output_path / experiment_name
+    base_path.mkdir(exist_ok=True, parents=True)
+
+    model = TileNet.load_from_checkpoint(checkpoint, dim=dim)
     df = pd.read_parquet(parquet)
     df["species"] = df.source_name.apply(lambda x: x.split("/")[1])
     df[["species"]].groupby("species").size().sort_values(ascending=False)
 
-    data = []
-    for row in df[df.species == intra].sample(50).itertuples():
-        y = motif.load_audio(root / row.source_name, int(row.motif_0), 5)
-        data.append(y)
+    print("Intraclass Figures")
+    intra_figures = []
+    for specie in species:
+        print(f"calculating figures for {specie}")
+        intra_figures += intra_cluster(base_path, root, df, model, specie)
 
-    z = data[0]
-    plt.plot(z)
-    os.makedirs(f"{outputdir}/{name}", exist_ok=True)
-    plt.savefig(f"{outputdir}/{name}/{intra}1.png", bbox_inches="tight")
-    intrafigures = [f"{outputdir}/{name}/{intra}1.png"]
-    plt.clf()
-
-    emb = model(torch.from_numpy(np.array(data))).detach().numpy()
-
-    g = PCA(n_components=2).fit_transform(emb)
-    plt.scatter(g[:, 0], g[:, 1])
-
-    plt.savefig(f"{outputdir}/{name}/{intra}2.png", bbox_inches="tight")
-    intrafigures.append(f"{outputdir}/{name}/{intra}2.png")
-    plt.clf()
-
-    kmeans = KMeans(n_clusters=3).fit(emb)
-    kmeans.labels_
-    plt.scatter(g[:, 0], g[:, 1], c=kmeans.labels_)
-    plt.savefig(f"{outputdir}/{name}/{intra}3.png", bbox_inches="tight")
-    intrafigures.append(f"{outputdir}/{name}/{intra}3.png")
-    plt.clf()
-    print("Created Intraclass Figures")
-
-    # Interclass Clustering and Comparison
-    data = []
-    labels = []
-    for row in df[df.species.isin(inter.split(","))].sample(300).itertuples():
-        y = motif.load_audio(root / row.source_name, int(row.motif_0), 5)
-        data.append(y)
-        labels.append(row.species)
-
-    for i in range(3):
-        z = data[i]
-        plt.plot(z)
-        plt.clf()
-
-    emb = model(torch.from_numpy(np.array(data))).detach().numpy()
-    le = LabelEncoder().fit(labels)
-
-    g = PCA(n_components=2).fit_transform(emb)
-    plt.scatter(g[:, 0], g[:, 1], c=le.transform(labels))
-    store = f"{outputdir}/{name}/{inter}1.png"
-    plt.savefig(store, bbox_inches="tight")
-    plt.clf()
-    print("Created Interclass Figures")
+    print("Interclass Figures")
+    inter_figures = inter_cluster(base_path, root, df, model, species)
 
     # Create html file with Jinja2 template
     env = Environment(
@@ -123,16 +180,13 @@ def main(intra, inter, checkpoint, parquet, outputdir, root, dim, name):
     )
     template = env.get_template("index.html.j2")
     output = template.render(
-        intraspecies=intra,
-        interspecies=inter,
-        intrafigures=intrafigures,
-        interfigures=store,
+        species=species,
+        intra_figures=intra_figures,
+        inter_figures=inter_figures,
     )
-
-    with open(f"{outputdir}/{name}/index.html", "w") as fp:
-        fp.write(output)
-    webbrowser.open_new(f"{outputdir}/{name}/index.html")
+    (base_path / "index.html").write_text(output)
+    webbrowser.open_new(f"{base_path}/index.html")
 
 
 if __name__ == "__main__":
-    main()
+    evaluation()
